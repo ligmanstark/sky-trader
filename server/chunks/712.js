dd5e78e3eac2c6c8f42d4b142ec6c5ad8a3a042b
levis <miduwaystark@gmail.com>
2023-12-28 22:22:59 +0400

    Updates

---
+++ server/chunks/712.js
@@ -0,0 +1,43 @@
+"use strict";exports.id=712,exports.ids=[712],exports.modules={1323:(e,r)=>{Object.defineProperty(r,"l",{enumerable:!0,get:function(){return function e(r,t){return t in r?r[t]:"then"in r&&"function"==typeof r.then?r.then(r=>e(r,t)):"function"==typeof r&&"default"===t?r:void 0}}})},6041:(e,r,t)=>{Object.defineProperty(r,"__esModule",{value:!0}),Object.defineProperty(r,"default",{enumerable:!0,get:function(){return u}});let o=t(167),n=o._(t(6689)),d=t(1687);async function i(e){let{Component:r,ctx:t}=e,o=await (0,d.loadGetInitialProps)(r,t);return{pageProps:o}}class u extends n.default.Component{render(){let{Component:e,pageProps:r}=this.props;return n.default.createElement(e,r)}}u.origGetInitialProps=i,u.getInitialProps=i,("function"==typeof r.default||"object"==typeof r.default&&null!==r.default)&&void 0===r.default.__esModule&&(Object.defineProperty(r.default,"__esModule",{value:!0}),Object.assign(r.default,r),e.exports=r.default)},4684:(e,r,t)=>{t(997)},8733:(e,r,t)=>{t.a(e,async(e,r)=>{try{var o=t(4115),n=e([o]);o=(n.then?(await n)():n)[0],o.default.button`
+	color: ${({$color:e})=>e?"#fff":"#000"};
+	background-color: ${({$color:e})=>e?"#009EE4":"#fff"};
+	width: ${({$width:e})=>e?`${e}rem`:"17rem"};
+	min-width: 17rem;
+	padding: 0.75rem 1rem 1rem;
+	font-family: inherit;
+	font-size: 1.25rem;
+	border-radius: 1rem;
+	cursor: pointer;
+	border: ${({$border:e})=>e?"0.0625rem solid #D0CECE":null};
+	&:hover {
+		background: ${({$color:e})=>e?"#0080C1":"#009EE4"};
+	}
+	&:active {
+		background: ${({$color:e})=>e?"#009EE4":"##0080C1"};
+	}
+	&:disabled {
+		cursor: not-allowed;
+		background-color: #D9D9D9;
+	}
+`,o.default.button`
+	color: ${({$color:e})=>e?"#fff":"#000"};
+	background-color: ${({$color:e})=>e?"#009EE4":"#fff"};
+	width: ${({$width:e})=>e?`${e}rem`:"17rem"};
+	min-width: 17rem;
+	padding: 0.75rem 1rem 1rem;
+	font-family: inherit;
+	font-size: 1.25rem;
+	border-radius: 1rem;
+	cursor: pointer;
+	border: ${({$border:e})=>e?"0.0625rem solid #D0CECE":null};
+	// &:hover {
+	// 	background: ${({$color:e})=>e?"#0080C1":"#009EE4"};
+	// }
+	&:active {
+		background: ${({$color:e})=>e?"#009EE4":"##0080C1"};
+	}
+	&:disabled {
+		cursor: not-allowed;
+		background-color: #D9D9D9;
+	}
+`,r()}catch(e){r(e)}})},9403:(e,r)=>{var t;Object.defineProperty(r,"x",{enumerable:!0,get:function(){return t}}),function(e){e.PAGES="PAGES",e.PAGES_API="PAGES_API",e.APP_PAGE="APP_PAGE",e.APP_ROUTE="APP_ROUTE"}(t||(t={}))}};
---
+++ server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"wqxO+s4upwEvUYy7Cs+MXxw0Sd7f8TEQ28Jk1KBd3pI=\"}"
